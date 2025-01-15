de4ae31e3d557c609181cae0701438481d025866
s777enx <agent.mp33@mail.ru>
2025-01-15 17:13:31 +0300

    Added code files bring to front
    For easier read

---
+++ ObjectFreeSlot.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class ObjectFreeSlot : MonoBehaviour
+{
+    // Любой скрипт для слота тут
+    // Скрипт служит для обозначения всех слотов и возможности скриптинга на них
+}
---
+++ ObjectSlot.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class ObjectSlot : MonoBehaviour
+{
+    // Любой скрипт для слота тут
+    // Скрипт служит для обозначения всех слотов и возможности скриптинга на них
+}
